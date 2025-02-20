cd4321c1459c684cb697ecf742fa1657f3c142f2
guy-mel <guy.melamed@remepy.com>
2025-02-20 13:15:43 +0200

    Change default background color

--- lib/src/subtitles/better_player_subtitles_configuration.dart
+++ lib/src/subtitles/better_player_subtitles_configuration.dart
@@ -50,7 +50,7 @@ class BetterPlayerSubtitlesConfiguration {
     this.rightPadding = 5.0,
     this.bottomPadding = 0.0,
     this.alignment = Alignment.center,
-    this.backgroundColor = Colors.transparent,
+    this.backgroundColor = const Color(0x99000000),
     this.textAlign = TextAlign.center,
   });
 }
